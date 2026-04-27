b873d62949f415c9177e54556ad7b9b16ee6216a
Sara Londreira <1251666@isep.ipp.pt>
2026-04-27 11:49:22 +0100

    Delete src directory

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import view.MainView;
-
-public class Main {
-    public static void main(String[] args) {
-        MainView vistaPrincipal = new MainView();
-        vistaPrincipal.iniciar();
-    }
-}
